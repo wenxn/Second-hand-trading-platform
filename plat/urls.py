9c96e9a3053a7182b8acff284156762baebb7c6c
wenxiaoni <wenxiaoni@guazi.com>
2019-01-08 10:11:42 +0800

    base update

--- plat/urls.py
+++ plat/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 
     re_path(r'^plat_(?P<pk>\d+)/$', views.plat_goods, name="plat_goods"),
     re_path(r'^plat_(?P<pk>\d+)/add_goods/$', views.add_goods, name="add_goods"),
-    #path('add_goods/',views.add_goods, name="add_goods"),
+    #path('goods/',views.plat_goods, name="plat_goods"),
     path('delete_goods/', views.delete_goods, name="delete_goods"),
 ]
